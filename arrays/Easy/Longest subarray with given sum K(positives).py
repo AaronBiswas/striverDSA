b1cc0d976f8dbd76f9199ed29bea387729ba8fa4
Aaron Biswas <savybiswas15@gmail.com>
2025-09-02 21:36:16 +0530

    Finding the longest subarray with a given sum K.

---
+++ arrays/Easy/Longest subarray with given sum K(positives).py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+
+#Brute Force
+
+# count=0
+# maxi=10**(-18)
+
+# for i in range(len(arr)):
+#     count=arr[i]
+#     for j in range(i,len(arr)):
+#         count+=arr[j]
+#         if(s==count):
+#             maxi=max((j-i)+1,maxi)
+#         elif(count>s):
+#             break
+
+#Better Solution (also works for negative integers and is the optimal case for it)
+       
+# hashed = defaultdict(int)
+# maxi=0
+# add=0
+
+# arr = [2,0,0,3]
+
+# k = int(input())
+
+# for i in range(len(arr)):
+#     add+=arr[i]
+#     if(add==k):
+#         maxi=max(maxi,i+1)
+  
+#     rem=add-k 
+#     if(rem in hashed.keys()):
+#         maxi=max(maxi,i-hashed[rem])
+#     if(add not in hashed.keys()):
+#         hashed[add]=i
+
+    
+# print(maxi)
+
+
+#Optimal Solution(Only for all positive integers)
+
+arr=[2,0,0,3]
+
+k=int(input())
+
+count=arr[0]
+maxi=0
+
+i=0
+j=0
+
+while( j<len(arr)):
+    while(i<=j and count>k ):
+        count-=arr[i]
+        i+=1
+    if(count==k):
+        maxi=max(maxi,j-i+1)
+    j+=1
+    if(j<len(arr)):
+        count+=arr[j]
+        
+        
+print(maxi)
