d00d045bd879f8621dee54260066850fc265e21e
Tushar Paul <tusharpaul7024@gmail.com>
2025-09-14 20:19:43 +0530

    added question comment in kadane algorithm

--- arrays/Medium/Maximum Subarray Sum.py
+++ arrays/Medium/Maximum Subarray Sum.py
@@ -1,28 +1,29 @@
-#Brute Force
-import sys
+# #Brute Force
+# import sys
 
-def maxSubarraySum(arr, n): 
-    maxi = -sys.maxsize - 1 # infinite negative
+# def maxSubarraySum(arr, n): 
+#     maxi = -sys.maxsize - 1 # infinite negative
 
-    for i in range(n):
-        sum = 0
-        for j in range(i, n):
-            # current subarray = arr[i.....j]
+#     for i in range(n):
+#         sum = 0
+#         for j in range(i, n):
+#             # current subarray = arr[i.....j]
 
-            #add the current element arr[j]
-            # to the sum i.e. sum of arr[i...j-1]
-            sum += arr[j]
+#             #add the current element arr[j]
+#             # to the sum i.e. sum of arr[i...j-1]
+#             sum += arr[j]
 
-            maxi = max(maxi, sum) # getting the maximum
+#             maxi = max(maxi, sum) # getting the maximum
 
-    return maxi
+#     return maxi
 
-arr = [ -2, 1, -3, 4, -1, 2, 1, -5, 4]
-n = len(arr)
-maxSum = maxSubarraySum(arr, n)
-print("The maximum subarray sum is:", maxSum)
+# arr = [ -2, 1, -3, 4, -1, 2, 1, -5, 4]
+# n = len(arr)
+# maxSum = maxSubarraySum(arr, n)
+# print("The maximum subarray sum is:", maxSum)
 
 #Optimal Solution
+#this solution is also for Print subarray with maximum subarray sum (extended version of above problem)
 arr=[-2,-3,4,-1,-2,1,5,-3]
 
 
